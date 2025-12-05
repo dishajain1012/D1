a3c9b2cc14b386c5ae971d3f51dd8aa889b77bd8
dishajain1012 <dishajain1020@gmail.com>
2025-12-05 08:39:43 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,5 +1,26 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+package pgmC1.sJSON;
+import java.io.FileReader;	
+
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ReJSON {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        JSONParser jsonparser = new JSONParser();
+        FileReader reader = new FileReader(".\\JSON\\student.json");
+
+        Object obj = jsonparser.parse(reader);
+        JSONObject studentobj = (JSONObject) obj;
+
+        String first = (String) studentobj.get("first");
+        String last = (String) studentobj.get("last");
+
+        System.out.println("Firstname: " + first);
+        System.out.println("Lastname: " + last);
     }
 }
